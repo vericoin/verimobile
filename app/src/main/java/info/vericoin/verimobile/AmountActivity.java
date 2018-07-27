fdc6dcb633d19fe72e202704edad39d769f29487
maxwell10206 <maxwell10206@sbcglobal.net>
2018-07-26 19:54:11 -0500

    Created two layouts, amount and recipient, and a new theme

---
+++ app/src/main/java/info/vericoin/verimobile/AmountActivity.java
@@ -0,0 +1,14 @@
+package info.vericoin.verimobile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AmountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amount);
+    }
+
+}
